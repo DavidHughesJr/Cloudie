6a760cd4a97bc6bc53cdff5f9886f6d6c4c7650a
DavidHughesJr <hughes014@yahoo.com>
2022-09-13 05:35:17 -0500

    testing

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { weatherApi } from "../services/weatherApi";
 import weatherSlice from "../services/weatherSlice";
 import { newsApi } from '../services/newsApi'
