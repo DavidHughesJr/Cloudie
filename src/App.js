7f9cc9dac2120b4d4d74da68dca02a492d813ffc
DavidHughesJr <hughes014@yahoo.com>
2022-09-13 11:36:50 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import LeftNav from './components/LeftNav'
 import { theme } from './theme/theme'
 import { DashboardContainer, MiddleContainer } from './theme/containers';
-// import { Routes, Route } from 'react-router-dom'
-// import { Dashboard, Map, Saves } from './pages/allPages'
+import { Routes, Route } from 'react-router-dom'
+import { Dashboard, Map, Saves } from './pages/allPages'
 import TopNav from './components/TopNav';
 import WeatherDisplay from './components/WeatherDisplay'
 
@@ -18,11 +18,11 @@ const App = () => {
         <DashboardContainer>
           <LeftNav />
           <MiddleContainer>
-            {/* <Routes>
+            <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/map" element={<Map />} />
               <Route path="/saves" element={<Saves />} />
-            </Routes> */}
+            </Routes>
           </MiddleContainer>
           <WeatherDisplay />
         </DashboardContainer>
