8061b6a13166cbf15c59ddad099db653618fc6b9
DavidHughesJr <hughes014@yahoo.com>
2022-09-13 20:08:08 -0500

    test env

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,19 +1,20 @@
-import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 
 const baseUrl = `https://weatherapi-com.p.rapidapi.com`
 
 const weatherHeaders = {
-        'X-RapidAPI-Key': '419bd5942fmshff5dd7dcf34cd06p1dd32djsne642e068e809',
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
+    'X-RapidAPI-Key': process.env.REACT_APP_WEATHER_API_KEY
+    ,
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+}
 
-const requestWeather = (url) => ({url, headers: weatherHeaders})
+const requestWeather = (url) => ({ url, headers: weatherHeaders })
 
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
+    baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getForecastWeather: builder.query({
             query: (location) => requestWeather(`/forecast.json?q=${location}&days=3`)
